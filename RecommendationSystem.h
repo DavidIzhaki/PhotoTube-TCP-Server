a46acea09c1abec66b4cf250376f861c4dc46527
romGreen <rom149923@gmail.com>
2024-09-07 03:31:33 +0300

    rom push

--- RecommendationSystem.h
+++ RecommendationSystem.h
@@ -9,8 +9,9 @@ class RecommendationSystem {
 public:
     RecommendationSystem();
     ~RecommendationSystem();
-    std::vector<std::string> getRecommendations(const std::string& userId, const std::string& videoId);
+
     void updateHistory(const std::string& userId, const std::string& videoId);
+    std::vector<std::string> getRecommendations(const std::string& userId, const std::string& videoId);
 
 private:
     std::vector<std::string> getRandomVideos(int count, const std::set<std::string>& exclude);
